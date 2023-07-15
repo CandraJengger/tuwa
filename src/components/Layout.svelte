10c44e108e9a7b3da2e1212976ad0f26af577884
CandraJengger <cakjengger@gmail.com>
2023-07-16 04:07:47 +0700

    feat: Add metadata

--- src/components/Layout.svelte
+++ src/components/Layout.svelte
@@ -1,7 +1,7 @@
 <main
   class="flex h-screen w-screen items-center justify-center bg-white px-4 dark:bg-slate-900"
 >
-  <section class="mx-auto w-full max-w-md">
+  <section class="mx-auto w-full max-w-md -translate-y-[100px]">
     <slot />
   </section>
 </main>
