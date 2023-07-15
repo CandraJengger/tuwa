27ee3f0654eb6fd57a3bc64a1d918e2c16581163
CandraJengger <cakjengger@gmail.com>
2023-07-16 04:28:00 +0700

    fix: Styles

--- src/components/Layout.svelte
+++ src/components/Layout.svelte
@@ -1,7 +1,7 @@
 <main
-  class="flex h-screen w-screen items-center justify-center bg-white px-4 dark:bg-slate-900"
+  class="flex h-screen w-screen items-start justify-center bg-white px-4 pt-[32px] dark:bg-slate-900 md:items-center"
 >
-  <section class="mx-auto w-full max-w-md -translate-y-[100px]">
+  <section class="mx-auto w-full max-w-md">
     <slot />
   </section>
 </main>
